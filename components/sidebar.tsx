d5d7638286bbf6f62916e83e10ad83970a9c4885
Tanay Kumar <tanay25000@gmail.com>
2026-02-23 23:54:08 +0530

    fixed-sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 
-type MenuItem = { name: string; href: string; icon?: JSX.Element | string };
+type MenuItem = { name: string; href: string; icon?: React.ReactElement | string };
 
 interface SidebarProps {
     collapsed: boolean;
